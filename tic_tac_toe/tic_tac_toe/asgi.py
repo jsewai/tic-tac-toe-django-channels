b11f92c63a9ff84c656fd047a1881b7d071bc5ae
jsewai <junyasewai@gmail.com>
2021-04-01 21:29:18 -0400

    first commit

---
+++ tic_tac_toe/tic_tac_toe/asgi.py
@@ -0,0 +1,23 @@
+"""
+ASGI config for tic_tac_toe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+import django
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tic_tac_toe.settings')
+# django.setup()
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+    URLRouter
+  )
+})
